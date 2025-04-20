5fb4a24110f38aee3808e43eb9c136eb2b7729fa
joaofaccin <joaofaccin6@gmail.com>
2025-04-20 10:59:20 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia("111111", "Nokia 3310", "1234567890", "32GB");
+        Smartphone iphone = new Iphone("222222", "iPhone 14", "0987654321", "128GB");
+
+        Console.WriteLine("Usando Nokia:");
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine("\nUsando iPhone:");
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
